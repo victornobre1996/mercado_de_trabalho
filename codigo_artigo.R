ad19566f0093a1284614434c12f04dd96e29d3ca
LLeed <77404105+LLeed@users.noreply.github.com>
2021-11-16 23:09:52 -0300

    Update codigo_artigo.R
    * adicionado código rendimento por setor (total)

--- codigo_artigo.R
+++ codigo_artigo.R
@@ -26,7 +26,7 @@ pnad_2021_1 <- get_pnadc(year = 2021, quarter = 1, vars = var_select)
 pnad_2021_2 <- get_pnadc(year = 2021, quarter = 2, vars = var_select)
 
 
-# Combinando categorias - 
+# Combinando categorias - para compatibilização com o PIB Trimestral
 
 a <- pnad_2019
 e <-a$variables$VD4010[21] # "Administração pública, defesa e seguridade social "
@@ -62,8 +62,72 @@ pnad_2021_2$variables$VD4010[pnad_2021_2$variables$VD4010 == "Educação, saúde
 pnad_2021_2$variables$VD4010[pnad_2021_2$variables$VD4010 == "Serviços domésticos"] <- as.factor(f)
 pnad_2021_2$variables$VD4010[pnad_2021_2$variables$VD4010 == aea] <- as.factor(f) 
 
+## RENDIMENTO MEDIO POR SETOR ##
 
-# RENDIMENTO MEDIO POR SETOR E NIVEL DE INSTRUCAO
+pnad_rendimento_medio_setor_2019 <- svyby(formula =~VD4019, by = VD4010, design = pnad_2019, FUN = svymean, na.rm = TRUE )
+pnad_rendimento_medio_setor_2020_1 <- svyby(formula =~VD4019, by = VD4010, design = pnad_2020_1, FUN = svymean, na.rm = TRUE )
+pnad_rendimento_medio_setor_2020_2 <- svyby(formula =~VD4019, by = VD4010, design = pnad_2020_2, FUN = svymean, na.rm = TRUE )
+pnad_rendimento_medio_setor_2020_3 <- svyby(formula =~VD4019, by = VD4010, design = pnad_2020_3, FUN = svymean, na.rm = TRUE )
+pnad_rendimento_medio_setor_2020_4 <- svyby(formula =~VD4019, by = VD4010, design = pnad_2020_4, FUN = svymean, na.rm = TRUE )
+pnad_rendimento_medio_setor_2021_1 <- svyby(formula =~VD4019, by = VD4010, design = pnad_2021_1, FUN = svymean, na.rm = TRUE )
+pnad_rendimento_medio_setor_2021_2 <- svyby(formula =~VD4019, by = VD4010, design = pnad_2021_2, FUN = svymean, na.rm = TRUE )
+
+# adicionar os trimestres
+
+pnad_rendimento_medio_setor_2019 <- pnad_rendimento_medio_setor_2019 %>% mutate(trimestre = "4T/2019")
+pnad_rendimento_medio_setor_2020_1 <- pnad_rendimento_medio_setor_2020_1 %>% mutate(trimestre = "1T/2020")
+pnad_rendimento_medio_setor_2020_2 <- pnad_rendimento_medio_setor_2020_2 %>% mutate(trimestre = "2T/2020")
+pnad_rendimento_medio_setor_2020_3 <- pnad_rendimento_medio_setor_2020_3 %>% mutate(trimestre = "3T/2020")
+pnad_rendimento_medio_setor_2020_4 <- pnad_rendimento_medio_setor_2020_4 %>% mutate(trimestre = "4T/2020")
+pnad_rendimento_medio_setor_2021_1 <- pnad_rendimento_medio_setor_2021_1 %>% mutate(trimestre = "1T/2021")
+pnad_rendimento_medio_setor_2021_2 <- pnad_rendimento_medio_setor_2021_2 %>% mutate(trimestre = "2T/2021")
+
+# agregando as bases de rendimento medio
+
+pnad_rendimento_agregado_setor <- rbindlist(list(pnad_rendimento_medio_setor_2019,
+                                                 pnad_rendimento_medio_setor_2020_1,
+                                                 pnad_rendimento_medio_setor_2020_2,
+                                                 pnad_rendimento_medio_setor_2020_3,
+                                                 pnad_rendimento_medio_setor_2020_4,
+                                                 pnad_rendimento_medio_setor_2021_1,
+                                                 pnad_rendimento_medio_setor_2021_2))
+
+
+
+pnad_rendimento_agregado_setor <- pnad_rendimento_agregado_setor[,-"se"]
+
+
+#---- transformando rowname para variavel e separando ----
+
+pnad_rendimento_agregado_setor <- pnad_rendimento_agregado_setor %>% 
+  separate( 
+    col = "interaction(VD3004, VD4010)", 
+    into = c("instrucao", "setor"),
+    sep = "\\.")
+
+# organizar dataframe
+
+pnad_rendimento_agregado_setor <- pnad_rendimento_agregado_setor %>% select(trimestre,instrucao, setor, VD4019)
+
+pnad_rendimento_agregado_setor <- pnad_rendimento_agregado_setor %>% rename(rendimento_medio_nominal = VD4019)
+
+pnad_rendimento_agregado_setor$rendimento_medio_nominal <- round(pnad_rendimento_agregado_setor$rendimento_medio_nominal, digits = 2)
+
+## agregando os dados 
+
+pnad_rendimento_agregado_setor <- pnad_rendimento_agregado_setor %>% 
+  mutate(setor = case_when(setor == "Educação, saúde humana e serviços sociais" ~ 
+                             "Administração pública, defesa e seguridade social ",
+                           TRUE ~ setor))
+
+
+
+
+# ---- exportando a base no R ---- 
+write.csv(pnad_rendimento_agregado_setor, file = "pnad_rendimento_agregado_setor.csv")
+
+
+## RENDIMENTO MEDIO POR SETOR E NIVEL DE INSTRUCAO ##  
 
 pnad_rendimento_medio_2019 <- svyby(formula =~VD4019, by = ~interaction(VD3004,VD4010), design = pnad_2019, FUN = svymean, na.rm = TRUE )
 pnad_rendimento_medio_2020_1 <- svyby(formula =~VD4019, by = ~interaction(VD3004,VD4010), design = pnad_2020_1, FUN = svymean, na.rm = TRUE )
@@ -126,4 +190,10 @@ pnad_rendimento_agregado <- pnad_rendimento_agregado %>%
 
 # ---- exportando a base no R ---- 
 write.csv(pnad_rendimento_agregado, file = "Pnad_rendimento_agregado.csv")
+
+## ##
+
+
+
+
 
