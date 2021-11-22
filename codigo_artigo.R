8d9b2fb2f66550d8893883175b7eb4178c19df13
Ledson L. G. da Rosa <77404105+LLeed@users.noreply.github.com>
2021-11-22 18:38:33 -0300

    Update codigo_artigo.R
    * mudança classificação dos setores

--- codigo_artigo.R
+++ codigo_artigo.R
@@ -15,7 +15,9 @@ library(data.table)
 
 # baixando as bases
 
-var_select <- c("VD4019", "VD3004", "VD4010", "VD4009", "VD4012")
+var_select <- c("VD4002", "V2009", "VD4019", "VD3004", "VD4010", 
+                "VD4008", "VD4009", "VD4011" ,"VD4012")
+
 
 pnad_2019 <- get_pnadc(year = 2019, quarter = 4, vars = var_select)
 pnad_2020_1 <- get_pnadc(year = 2020, quarter = 1, vars = var_select)
@@ -28,43 +30,43 @@ pnad_2021_2 <- get_pnadc(year = 2021, quarter = 2, vars = var_select)
 
 # Combinando categorias - para compatibilização com o PIB Trimestral
 
+##Classificações de Referência##
 a <- pnad_2019
 e <-a$variables$VD4010[21] # "Administração pública, defesa e seguridade social "
+h <-a$variables$VD4010[40] # "Informação, comunicação e atividades financeiras, 
+#imobiliárias, profissionais e administrativas"
 f <-a$variables$VD4010[30] # "Outros Serviços"
+g <-a$variables$VD4008[1] # "Empregado no Setor Privado"
 aea <-a$variables$VD4010[10] # "Alojamento e alimentação"
 
+mylist <- list(pnad_2019, pnad_2020_1, pnad_2020_2, pnad_2020_3,
+               pnad_2020_4, pnad_2020_1, pnad_2021_2)
+mylist_1 <- list("2019","2020_1", "2020_2",
+                 "2020_3", "2020_4",
+                 "2021_1", "2021_2")
 
-pnad_2019$variables$VD4010[pnad_2019$variables$VD4010 == "Educação, saúde humana e serviços sociais"] <- as.factor(e)
-pnad_2019$variables$VD4010[pnad_2019$variables$VD4010 == "Serviços domésticos"] <- as.factor(f)
-pnad_2019$variables$VD4010[pnad_2019$variables$VD4010 == aea] <- as.factor(f) 
-
-pnad_2020_1$variables$VD4010[pnad_2020_1$variables$VD4010 == "Educação, saúde humana e serviços sociais"] <- as.factor(e)
-pnad_2020_1$variables$VD4010[pnad_2020_1$variables$VD4010 == "Serviços domésticos"] <- as.factor(f)
-pnad_2020_1$variables$VD4010[pnad_2020_1$variables$VD4010 == aea] <- as.factor(f) 
-
-pnad_2020_2$variables$VD4010[pnad_2020_2$variables$VD4010 == "Educação, saúde humana e serviços sociais"] <- as.factor(e)
-pnad_2020_2$variables$VD4010[pnad_2020_2$variables$VD4010 == "Serviços domésticos"] <- as.factor(f)
-pnad_2020_2$variables$VD4010[pnad_2020_2$variables$VD4010 == aea] <- as.factor(f) 
-
-pnad_2020_3$variables$VD4010[pnad_2020_3$variables$VD4010 == "Educação, saúde humana e serviços sociais"] <- as.factor(e)
-pnad_2020_3$variables$VD4010[pnad_2020_3$variables$VD4010 == "Serviços domésticos"] <- as.factor(f)
-pnad_2020_3$variables$VD4010[pnad_2020_3$variables$VD4010 == aea] <- as.factor(f) 
-
-pnad_2020_4$variables$VD4010[pnad_2020_4$variables$VD4010 == "Educação, saúde humana e serviços sociais"] <- as.factor(e)
-pnad_2020_4$variables$VD4010[pnad_2020_4$variables$VD4010 == "Serviços domésticos"] <- as.factor(f)
-pnad_2020_4$variables$VD4010[pnad_2020_4$variables$VD4010 == aea] <- as.factor(f)
-
-pnad_2021_1$variables$VD4010[pnad_2021_1$variables$VD4010 == "Educação, saúde humana e serviços sociais"] <- as.factor(e)
-pnad_2021_1$variables$VD4010[pnad_2021_1$variables$VD4010 == "Serviços domésticos"] <- as.factor(f)
-pnad_2021_1$variables$VD4010[pnad_2021_1$variables$VD4010 == aea] <- as.factor(f) 
-
-pnad_2021_2$variables$VD4010[pnad_2021_2$variables$VD4010 == "Educação, saúde humana e serviços sociais"] <- as.factor(e)
-pnad_2021_2$variables$VD4010[pnad_2021_2$variables$VD4010 == "Serviços domésticos"] <- as.factor(f)
-pnad_2021_2$variables$VD4010[pnad_2021_2$variables$VD4010 == aea] <- as.factor(f) 
-rm(a,e,f,aea,var_select)
-
-# na parte 1 e 2
-
+#Loop para a Substituição
+for (W in seq_along(mylist)){
+  
+  a <- mylist[[W]]
+  
+  # ***Separando Emprego Público e Privado em Adm. Pública(etc) e Outros (respectivamente) ***
+  a$variables$VD4010[a$variables$VD4010 == "Educação, saúde humana e serviços sociais" &
+                       a$variables$VD4008 == g ] <- as.factor(f)
+  a$variables$VD4010[a$variables$VD4010 == "Educação, saúde humana e serviços sociais" &
+                       a$variables$VD4008 != g ] <- as.factor(e)
+  # *** Agregando categorias em Outros ***
+  a$variables$VD4010[a$variables$VD4010 == h & 
+                       a$variables$VD4011 == "Diretores e gerentes" & 
+                       a$variables$VD4011 == "Profissionais das ciências e intelectuais"&
+                       a$variables$VD4011 == "Técnicos e profissionais de nível médio"&
+                       a$variables$VD4011 == "Trabalhadores de apoio administrativo"] <- as.factor(f)
+  a$variables$VD4010[a$variables$VD4010 == "Serviços domésticos"] <- as.factor(f)
+  a$variables$VD4010[a$variables$VD4010 == aea] <- as.factor(f)
+  
+  assign(paste0("pnad_1",mylist_1[[W]]),a);
+}
+rm(a,e,f,aea,h,g,var_select)
 
 
 #Ocupação - Total da Economia # Parte 3
@@ -74,12 +76,11 @@ mylist <- list(pnad_2019, pnad_2020_1, pnad_2020_2, pnad_2020_3,
 mylist_1 <-list("2019/4T", "2020/1T", "2020/2T", "2020/3T",
                 "2020/4T", "2021/1T", "2021/2T")
 
-for (h in seq_along(mylist_1)) {
   for (i in seq_along(mylist)) {
     p <- mylist[[i]]
     a<-as.data.frame(summary(
       na.omit(p$variables$VD4009))) %>% 
-      mutate(trimestre = mylist_1[[h]])
+      mutate(trimestre = mylist_1[[i]])
     
     a <- a %>% mutate(row.names(a)) 
     
@@ -87,7 +88,7 @@ for (h in seq_along(mylist_1)) {
       interaction((p$variables$VD4009),
                   (p$variables$VD4012),
                   drop = T)))) %>% 
-      mutate(trimestre = mylist_1[[h]])
+      mutate(trimestre = mylist_1[[i]])
     
     b <- b %>% mutate(row.names(b))
     
@@ -97,10 +98,10 @@ for (h in seq_along(mylist_1)) {
             "Empregador.Não contribuinte"),])
     
     d <- rbindlist(list(a,b), use.names=FALSE)
-    assign(paste0("pnad_ocupacao_",h), d)
+    assign(paste0("pnad_ocupacao_",i), d)
     rm(a,b,d);
   }
-}
+
 
 pnad_ocupacao_agregado <-rbindlist(list(pnad_ocupacao_1,
                                         pnad_ocupacao_2,
