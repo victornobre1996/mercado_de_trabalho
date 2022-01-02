b643021215cb2fdc9b1f1077e50411ae56a62cf0
Ledson L. G. da Rosa <77404105+LLeed@users.noreply.github.com>
2022-01-02 11:37:52 -0300

    Update rendimento_efetivo_ocupaco.R

--- rendimento_efetivo_ocupaco.R
+++ rendimento_efetivo_ocupaco.R
@@ -27,40 +27,43 @@ pnad_2021_1 <- get_pnadc(year = 2021, quarter = 1, vars = var_select, deflator =
 pnad_2021_2 <- get_pnadc(year = 2021, quarter = 2, vars = var_select, deflator = T)
 pnad_2021_3 <- get_pnadc(year = 2021, quarter = 3, vars = var_select, deflator = F)
 
-mylist <- list(pnad_2019, pnad_2020_1, pnad_2020_2, pnad_2020_3,
-               pnad_2020_4, pnad_2020_1, pnad_2021_2, pnad_2021_3)
-mylist_1 <-list("2019/4T", "2020/1T", "2020/2T", "2020/3T",
-                "2020/4T", "2021/1T", "2021/2T", "2021/3T")
+mylist <- list(pnad_2019) #, pnad_2020_1, pnad_2020_2, pnad_2020_3,
+               #pnad_2020_4, pnad_2020_1, pnad_2021_2, pnad_2021_3)
+mylist_1 <-list("2019/4T") #, "2020/1T", "2020/2T", "2020/3T",
+                #"2020/4T", "2021/1T", "2021/2T", "2021/3T")
 
 for (i in seq_along(mylist)) {
   p <- mylist[[i]]
-  a <- svyby(formula =~(VD4020_real = VD4020*Efetivo), by = ~VD4009, 
-             design = p, FUN = svymean, na.rm = TRUE )
-  a <- a %>% select(-c("Empregador", "Conta-própria"))  #qq coisa tira essa
+  #a <- svyby(formula =~(VD4020_real = VD4020*Efetivo), by = ~VD4009, 
+         #    design = p, FUN = svymean, na.rm = TRUE )
     
-  a1 <- svyby(formula =~(VD4020_real = VD4020*Efetivo), by = ~interaction(VD4009 == "Empregador" &
-                                                                   VD4012=="Contribuinte"), 
-              design = p, FUN = svymean, na.rm = TRUE )
+  a1 <- svyby(formula =~(VD4020_real = VD4020*Efetivo), by = ~interaction(VD4009 == "Empregador" & VD4012=="Contribuinte"), 
+              design = p, FUN = svymean, na.rm = TRUE, na.rm.all = T)
+
   
-  a2 <- svyby(formula =~(VD4020_real = VD4020*Efetivo), by = ~interaction(VD4009 == "Empregador" &
-                                                                   VD4012=="Não contribuinte"), 
-              design = p, FUN = svymean, na.rm = TRUE )
+  #a2 <- svyby(formula =~(VD4020_real = VD4020*Efetivo), by = ~interaction(VD4009 == "Empregador" &
+           #                                                        VD4012=="Não contribuinte"), 
+           #   design = p, FUN = svymean, na.rm = TRUE )
   
-  a3 <- svyby(formula =~(VD4020_real = VD4020*Efetivo), by = ~interaction(VD4009 == "Conta-própria" &
-                                                                   VD4012=="Contribuinte"),
-              design = p, FUN = svymean, na.rm = TRUE )
+ # a3 <- svyby(formula =~(VD4020_real = VD4020*Efetivo), by = ~interaction(VD4009 == "Conta-própria" &
+           #                                                        VD4012=="Contribuinte"),
+         #     design = p, FUN = svymean, na.rm = TRUE )
   
-  a4 <- svyby(formula =~(VD4020_real = VD4020*Efetivo), by = ~interaction(VD4009 == "Conta-própria" &
-                                                                   VD4012=="Não contribuinte"),
-              design = p, FUN = svymean, na.rm = TRUE )
-  
+ # a4 <- svyby(formula =~(VD4020_real = VD4020*Efetivo), by = ~interaction(VD4009 == "Conta-própria" &
+           #                                                        VD4012=="Não contribuinte"),
+           #   design = p, FUN = svymean, na.rm = TRUE )
               
-  d <- rbindlist(list(a,a1,a2,a3,a4), use.names=FALSE) %>% 
-    mutate(trimestre = mylist_1[[i]])
-  
-  assign(paste0("pnad_rendiento_ocupados",i), d)            
+  #d <- rbindlist(list(a,a1,a2,a3,a4), use.names=FALSE) %>% 
+   # mutate(trimestre = mylist_1[[i]])
+
+  assign(paste0("pnad_rendiento_ocupados_ok",i), a1)            
   
 }
 
 ##falta resto do código
+
+
+interaction(pnad_2019$variables$VD4009 == "Empregador" &
+              pnad_2019$variables$VVD4012 =="Contribuinte", drop = T)
+
 
