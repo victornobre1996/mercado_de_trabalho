e05124d86abca2c5be839c08d9ce1f94fb0c582a
Victor Nobre <victornobre78@hotmail.com>
2022-01-05 15:32:47 -0300

    foi adicionado o rendimento para o trabalho principal e a variavel de contribuinte e n contribuinte

--- rendimento_efetivo_ocupaco.R
+++ rendimento_efetivo_ocupaco.R
@@ -15,7 +15,7 @@ library(data.table)
 
 
 var_select <- c("VD4002", "V2009", "VD4019", "VD4020","VD3004", "VD4010", 
-                "VD4008", "VD4009", "VD4011" ,"VD4012")
+                "VD4008", "VD4009", "VD4011" ,"VD4012", "V4032", "VD4017")
 
 
 pnad_2019 <- get_pnadc(year = 2019, quarter = 4, vars = var_select, deflator = T)
@@ -34,33 +34,64 @@ mylist_1 <-list("2019/4T", "2020/1T", "2020/2T", "2020/3T",
 
 for (i in seq_along(mylist)) {
   p <- mylist[[i]]
-  a <- svyby(formula =~(VD4020_real = VD4020*Efetivo), by = ~VD4009, 
+  
+  a <- svyby(formula =~(VD4017_real = VD4017*Efetivo), by = ~VD4009, 
              design = p, FUN = svymean, na.rm = TRUE )
-  a <- a %>% select(-c("Empregador", "Conta-própria"))  #qq coisa tira essa
     
-  a1 <- svyby(formula =~(VD4020_real = VD4020*Efetivo), by = ~interaction(VD4009 == "Empregador" &
-                                                                   VD4012=="Contribuinte"), 
+  a1 <- svyby(formula =~(VD4017_real = VD4017*Efetivo), by = ~interaction(VD4009 == "Empregador" &
+                                                                   V4032== "Sim"), 
               design = p, FUN = svymean, na.rm = TRUE )
   
-  a2 <- svyby(formula =~(VD4020_real = VD4020*Efetivo), by = ~interaction(VD4009 == "Empregador" &
-                                                                   VD4012=="Não contribuinte"), 
+  a2 <- svyby(formula =~(VD4017_real = VD4017*Efetivo), by = ~interaction(VD4009 == "Empregador" &
+                                                                   V4032=="Não"), 
               design = p, FUN = svymean, na.rm = TRUE )
   
-  a3 <- svyby(formula =~(VD4020_real = VD4020*Efetivo), by = ~interaction(VD4009 == "Conta-própria" &
-                                                                   VD4012=="Contribuinte"),
+  a3 <- svyby(formula =~(VD4017_real = VD4017*Efetivo), by = ~interaction(VD4009 == "Conta-própria" &
+                                                                   V4032=="Sim"),
               design = p, FUN = svymean, na.rm = TRUE )
   
-  a4 <- svyby(formula =~(VD4020_real = VD4020*Efetivo), by = ~interaction(VD4009 == "Conta-própria" &
-                                                                   VD4012=="Não contribuinte"),
+  a4 <- svyby(formula =~(VD4017_real = VD4017*Efetivo), by = ~interaction(VD4009 == "Conta-própria" &
+                                                                   V4032=="Não"),
               design = p, FUN = svymean, na.rm = TRUE )
   
               
   d <- rbindlist(list(a,a1,a2,a3,a4), use.names=FALSE) %>% 
     mutate(trimestre = mylist_1[[i]])
   
-  assign(paste0("pnad_rendiento_ocupados",i), d)            
+  assign(paste0("pnad_rendimento_ocupados",i), d)            
   
 }
 
-##falta resto do código
+# agregando as bases
+
+pnad_rendimento_agregado_ocupacao <- rbindlist(list(pnad_rendimento_ocupados1,
+                                                    pnad_rendimento_ocupados2,
+                                                    pnad_rendimento_ocupados3,
+                                                    pnad_rendimento_ocupados4,
+                                                    pnad_rendimento_ocupados4,
+                                                    pnad_rendimento_ocupados5,
+                                                    pnad_rendimento_ocupados6,
+                                                    pnad_rendimento_ocupados7))
+
+# tratando os dados
+
+pnad_rendimento_agregado_ocupacao_1 <- pnad_rendimento_agregado_ocupacao %>% 
+  select(trimestre,VD4009,`VD4020_real = VD4020 * Efetivo`)
+
+
+# 
+
+pnad_rendimento_agregado_ocupacao_1 <-pnad_rendimento_agregado_ocupacao_1 %>% 
+  rename(posicao_ocupacao = VD4009,
+         rendimento_efetivo = `VD4020_real = VD4020 * Efetivo`)
+
+
+
+pnad_rendimento_agregado_ocupacao_1$rendimento_efetivo <- round(pnad_rendimento_agregado_ocupacao_1$rendimento_efetivo, digits = 2)
+
+
+write.csv(pnad_rendimento_agregado_ocupacao_1, file = "pnad_ocupacao.csv")
+
+
+
 
